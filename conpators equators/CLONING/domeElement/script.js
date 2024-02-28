6f0f939d54b831cfeb290ffd0a85a63e2bc8ce8b
basirat10 <basiratamusat10@gmail.com>
2024-02-28 13:58:09 +0100

    date

---
+++ conpators equators/CLONING/domeElement/script.js
@@ -0,0 +1,19 @@
+const tegEl = document.getElementsByTagName('h1');
+console.log(tegEl)
+const tagEl = document.getElementsByTagName('texting');
+console.log(tagEl);
+
+const checkId = document.getElementById('boxText');
+
+const allEl = document.querySelectorAll('selectEl');
+
+const oneEl = document.querySelector('selectElOne');
+
+const ell = document.querySelector('peleZainab');
+
+const submitBtn = document.querySelector('button');
+console.log(submitBtn)
+
+submitBtn.addEventListener('moseover',()=>{
+    alert('Its closing time already')
+})
